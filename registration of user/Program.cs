32eb1507728f8364c3cec2e1bfd0c631abb1bf8f
mani1 <mani1@NANI143>
2022-07-08 20:45:46 +0530

    user registration

---
+++ registration of user/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+
+public class MainClass
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine(validateFirstName("Tom"));
+		Console.WriteLine(validateLastName("Tom"));
+	}
+	// validate first name
+	public static bool validateFirstName(string firstName)
+	{
+		return firstName.matches("[A-Z][a-zA-Z]*");
+	} // end method validateFirstName
+
+	// validate last name
+	public static bool validateLastName(string lastName)
+	{
+		return lastName.matches("[a-zA-z]+([ '-][a-zA-Z]+)*");
+	} // end method validateLastName
+
+}
---
+++ user email id/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace EmailAddressValidationConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var emailAddresses = new List<string>
+            {
+                // Valid email addresses based on regex defined
+                "raji@example.com",
+                "raji.balusupati@example.net",
+                "raji.balusupati@example.gov",
+                "RAJI@BALUSUPATI.GOV",
+
+                // Invalid email addresses based on regex defined
+                "raji@example",
+                "rajiexample.co.uk",
+                "raji@example.me"
+            };
+
+            foreach (var emailAddress in emailAddresses)
+            {
+                Console.WriteLine($"{emailAddress} is" + (IsValid(emailAddress) ? " a valid" : " an invalid") + " email address.");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static bool IsValid(string email)
+        {
+            string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$";
+
+            return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+        }
+    }
+}
